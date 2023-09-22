1aeedd32f0a44b488ccc6e085301f2251d946192
lraraujo <lucasregoaraujo@gmail.com>
2023-09-21 21:18:57 -0300

    Section 4 completed. Pages, Layouts and Routing.

--- WiredBrainCoffeeAdmin/Pages/Index.cshtml.cs
+++ WiredBrainCoffeeAdmin/Pages/Index.cshtml.cs
@@ -13,6 +13,7 @@ namespace WiredBrainCoffeeAdmin.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public List<SurveyItem> SurveyResults { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -21,7 +22,8 @@ namespace WiredBrainCoffeeAdmin.Pages
 
         public void OnGet()
         {
-
+            var rawJson = System.IO.File.ReadAllText("wwwroot/sampledata/survey.json");
+            SurveyResults = JsonSerializer.Deserialize<List<SurveyItem>>(rawJson);
         }
     }
 }
